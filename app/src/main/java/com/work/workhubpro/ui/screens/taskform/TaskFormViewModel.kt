23100a6c235fa68824aace97bc99003974cfa057
Aditya Trivedi <adit4443ya@gmail.com>
2024-04-25 08:18:05 +0530

    Added dropdown functionality

--- app/src/main/java/com/work/workhubpro/ui/screens/taskform/TaskFormViewModel.kt
+++ app/src/main/java/com/work/workhubpro/ui/screens/taskform/TaskFormViewModel.kt
@@ -2,9 +2,9 @@ package com.work.workhubpro.ui.screens.taskform
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.work.workhubpro.SharedViewModel
 import com.work.workhubpro.models.Task
 import com.work.workhubpro.repository.TaskRepository
+import com.work.workhubpro.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -12,21 +12,33 @@ import javax.inject.Inject
 @HiltViewModel
 public class TaskFormViewModel @Inject constructor(
     private val repo: TaskRepository,
-    private val sharedViewModel: SharedViewModel
+    private val users: UserRepository
 ) : ViewModel() {
-    fun createTask(name: String, description: String, assignedTo: String) {
+    fun createTask(name: String, description: String, assignedTo: String,assignedby:Int) {
         println(name)
-        if (sharedViewModel.user.value != null) {
             val task = Task(
                 name = name,
                 description = description,
                 assigned_to = assignedTo,
-                assigned_by = sharedViewModel.user.value!!.ID!!,
+                assigned_by = assignedby,
                 status = "notStarted"
             ) // Create a new Task instance
             viewModelScope.launch {
                 repo.createTask(task)
             }
+
+    }
+
+    val project_lead=users.projectleaders
+    val employees=users.employees
+    fun getProjectLeaders(workhub_id:Int){
+        viewModelScope.launch {
+            users.projectLead_from_workhub(workhub_id)
+        }
+    }
+    fun getemployees(workhub_id:Int){
+        viewModelScope.launch {
+            users.employees_from_workhub(workhub_id)
         }
     }
 }
